6483f421b74746a9f0104740e3a42adcf5ecd5c3
Anna Litovskaya <Litovskaya09@gmail.com>
2021-02-26 19:28:16 -0500

    database typo fix

--- db/seed.js
+++ db/seed.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize(
-  process.send.DATABASE_URL || 'postgres://localhost/cat_shelter_db',
+  process.env.DATABASE_URL || 'postgres://localhost/cat_shelter_db',
   { logging: false }
 );
 const { DataTypes } = Sequelize;
